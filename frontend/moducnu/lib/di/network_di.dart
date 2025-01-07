716b0ca6ec78317ad281a3448d3bd08ed4d963c8
이준구 <yijungu@ijunguui-noteubug.local>
2025-01-07 21:46:19 +0900

    배포 버전

--- frontend/moducnu/lib/di/network_di.dart
+++ frontend/moducnu/lib/di/network_di.dart
@@ -10,9 +10,9 @@ void setupNetworkDependencies() {
   String baseUrl;
 
   if (Platform.isAndroid) {
-    baseUrl = "http://10.0.2.2:8000/"; // Android 에뮬레이터
+    baseUrl = "http://15.164.210.125/"; // Android 에뮬레이터
   } else if (Platform.isIOS) {
-    baseUrl = "http://127.0.0.1:8000/"; // iOS 시뮬레이터
+    baseUrl = "http://15.164.210.125/"; // iOS 시뮬레이터
   } else {
     throw UnsupportedError("이 플랫폼은 지원되지 않습니다."); // 기타 플랫폼 예외 처리
   }
